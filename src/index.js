60456251a99c7a0d7284df2828f0de43d7646cb1
mixinspace <islammadkaur@gmail.com>
2020-12-13 13:37:23 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const newForm = document.querySelector('#create-task-form')
+  newForm.addEventListener("submit", newTask)
+})
+
+const newTask = event => {
+  event.preventDefault()
+
+  const li = document.createElement('li')
+  const desc = document.querySelector('#new-task-description')
+  const task = document.querySelector('#tasks')
+  li.innerText = desc.value
+
+  task.append(li)
+
+  event.target.reset();
+}
